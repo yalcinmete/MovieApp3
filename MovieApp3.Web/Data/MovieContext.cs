26dcd69963788d0678cd145d3e5cbcabdac81f3e
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-16 17:23:59 +0300

    MovieContext- MovieContext sınıfı oluşturuldu

---
+++ MovieApp3.Web/Data/MovieContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp3.Web.Entity;
+using System;
+
+namespace MovieApp3.Web.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+            //startuptaki optionları base gönderdik.
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Person> People { get; set; }
+        //public DbSet<Crew> Crews { get; set; }
+        //public DbSet<Cast> Casts { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("Data Source=movie.db");
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Movie>().Property(b => b.Title).IsRequired();
+        //    modelBuilder.Entity<Movie>().Property(b => b.Title).HasMaxLength(500);
+        //    modelBuilder.Entity<Genre>().Property(b => b.Name).IsRequired();
+        //    modelBuilder.Entity<Genre>().Property(b => b.Name).HasMaxLength(50);
+        //}
+    }
+}
--- MovieApp3.Web/Startup.cs
+++ MovieApp3.Web/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieApp3.Web.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,12 +16,21 @@ namespace MovieApp3.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddControllers(); //API icin.
-            //services.AddRazorPages(); //Razor sayfalar için.
+            //services.AddRazorPages(); //Razor sayfalar için.,
+
+            services.AddDbContext<MovieContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("MsSQLConnection")));
+            options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews(); //MVC kullanýmý icin.
         }
