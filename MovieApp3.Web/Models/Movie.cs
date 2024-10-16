58f71d752ac6a5c4fbb755e977ea0705374af75a
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-16 09:36:41 +0300

    Form Validations islemleri yapıldı

--- MovieApp3.Web/Models/Movie.cs
+++ MovieApp3.Web/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieApp3.Web.Models
 {
@@ -6,12 +7,16 @@ namespace MovieApp3.Web.Models
     {
         public int MovieId { get; set; }
         [DisplayName("Başlık")]
-        public string Title { get; set; }
+        [Required(ErrorMessage = "film başlığı eklemelisiniz")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "film başlığı 5-10 karakter aralığında olmalıdır.")]
+        public string Title { get; set; } //null
+        [Required(ErrorMessage = "film açıklaması eklemelisiniz")]
         public string Description { get; set; }
         public string Director { get; set; }
         public string[] Players { get; set; }
-        public string ImageUrl { get; set; }
-
-        public int GenreId { get; set; }
+        [Required]
+        public string ImageUrl { get; set; } //string değer boş değer için null alır
+        [Required]
+        public int? GenreId { get; set; } //int boş değer için 0 alır.Required demiş olsak bile boş iken 0 değeri atandığı için Required işlevsiz olur. Bu nedenle int? diyip boş ise null atansın diyoruz.
     }
 }
