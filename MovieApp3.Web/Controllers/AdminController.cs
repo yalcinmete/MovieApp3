37878b18e89bc86090180b87b005295cd2b119d4
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-25 10:29:03 +0300

    AdminPaneli_FilmListesi - Filmler listelendi.

---
+++ MovieApp3.Web/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieApp3.Web.Data;
+using MovieApp3.Web.Models;
+using System.Linq;
+
+namespace MovieApp3.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly MovieContext _context;
+
+        public AdminController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieList()
+        {
+            ////Daha önce AdminMoviesViewModel ile içerisinde Movie dönüyorduk.
+            //return View(new AdminMoviesViewModel
+            //{
+            //    Movies = _context.Movies.Include(m => m.Genres).ToList()
+            //});
+
+
+            return View(new AdminMoviesViewModel
+            {
+                Movies = _context.Movies
+                    .Include(m=>m.Genres)
+                    .Select(m=>new AdminMovieViewModel
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ImageUrl = m.ImageUrl,
+                        Genres = m.Genres.ToList()
+                    })
+                    .ToList()
+            });
+        }
+    }
+}
---
+++ MovieApp3.Web/Models/AdminMoviesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MovieApp3.Web.Entity;
+using System.Collections.Generic;
+
+namespace MovieApp3.Web.Models
+{
+    public class AdminMoviesViewModel
+    {
+        //public List<Movie> Movies { get; set; }
+        public List<AdminMovieViewModel> Movies { get; set; } //Açıklama bilgisi gelmesin istedik
+    }
+
+    public class AdminMovieViewModel //Açıklama bilgisi gelmesin istedik özelleştirilmiş class oluşturduk.
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
