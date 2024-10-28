f14eeeb15b3739fd4a5c3a56a68f29c8eb67a72b
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-28 14:08:20 +0300

    AdminPaneli_TurListesi - Admin tür listesi sayfası oluşturuldu

--- MovieApp3.Web/Models/AdminGenresViewModel.cs
+++ MovieApp3.Web/Models/AdminGenresViewModel.cs
@@ -15,4 +15,11 @@ namespace MovieApp3.Web.Models
         public string Name { get; set; }
         public int Count { get; set; }
     }
+
+    public class AdminGenreEditViewModel
+    {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public List<AdminMovieViewModel> Movies { get; set; }
+    }
 }
