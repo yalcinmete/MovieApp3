5a81ef5df0f078bfa6d7ab440f3ba0c6a7ee5d32
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-30 14:25:15 +0300

    FormValidations_KullanıcıEklemeFormuValidate-Kullanıcı ekleme (user/createuser) formuna validation ekledik.

--- MovieApp3.Web/Models/UserModel.cs
+++ MovieApp3.Web/Models/UserModel.cs
@@ -7,13 +7,31 @@ namespace MovieApp3.Web.Models
     public class UserModel
     {
         public int UserId { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "UserName için 10 karakterden fazla olamaz")]
         public string UserName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "{0} karakter uzunluğu {2}-{1} arasında olmalıdır", MinimumLength = 3)]
         public string Name { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
         public string RePassword { get; set; }
+
+        [Url]
         public string Url { get; set; }
-        public DateTime BirthDate { get; set; }
+
+        [Range(1900, 2010)]
+        public int BirthYear { get; set; }
     }
 }
