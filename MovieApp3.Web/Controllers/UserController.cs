bed409aec8e4e0813574545ad52acd2561e7a399
yalcinmete <yalcinmete.g4@gmail.com>
2024-10-30 14:00:00 +0300

    FormValidations_KullanıcıEklemeFormu-Kullanıcı Ekleme sayfası olusturuldu.

---
+++ MovieApp3.Web/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp3.Web.Models;
+
+namespace MovieApp3.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieApp3.Web/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MovieApp3.Web.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RePassword { get; set; }
+        public string Url { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
